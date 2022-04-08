669e67f49748229a5cf8d701d92bfcd912439c65
Bilal Khan <b.khan98@gotmail.com>
2022-04-08 13:40:05 +0100

    added variables for python intro

---
+++ Variables/variables_intro.py
@@ -0,0 +1,65 @@
+# # a = 1
+# # b = 2
+# # c = 3.5
+# # d = "hello"
+# #
+# # print(a,b,c,d)
+# # print(c)
+# # print(type(a),type(c))
+#
+#
+#
+# a=125
+# b=1.25
+# c=3+4j
+#
+# d=a+c+b
+# print(d)
+#
+# a=10//2
+# print(a)
+# #
+# s="hello world!"
+# slice=s[6:9]
+# print(slice)
+# print(s[9])
+# hell=s[0:4]
+# print(hell)
+#
+# print(s[::-1])
+#
+# print(s.upper())
+#
+# n="Engineering 110"
+# print(n.strip('#'))
+#
+# print(s.replace('e'))
+#
+#
+#
+# a="here is"
+# b="a string"
+# c="in parts"
+# d=a+" "+ b+" "+ c
+# print(d.capitalize())
+#
+#
+# d=f"F-string: {a} {b} {c}."
+#
+# number=10
+# fruit="apples"
+# to_print="There are "+ str(number)+" "+fruit+"."
+# print(to_print)
+# to_print=f"F-string: {number} {fruit}."
+# print(to_print)
+#
+# print_f_string = f"There are {number} {fruit}."
+# print(print_f_string)
+
+name=input("What is your name?/n ")
+print(name)
+# \n means new line
+
+
+name_1=input("What is your name?\n ")
+print(name_1)
